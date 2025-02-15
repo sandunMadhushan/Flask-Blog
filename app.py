759ce9dba6b63b1406785eabe1cc39e0cbd47cc9
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2025-02-16 00:32:05 +0530

    added message when name form submitted

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -44,4 +44,6 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash('Form submitted successfully')
+        
     return render_template('name.html', name=name, form=form)
