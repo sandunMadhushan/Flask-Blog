58cb9eed7d15268a06948c911b71616d0aba1dd9
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2025-02-15 18:21:20 +0530

    added cutsom error pages

--- app.py
+++ app.py
@@ -13,3 +13,15 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', user_name=name)
+
+# Create custom error pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# Internal server error
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
