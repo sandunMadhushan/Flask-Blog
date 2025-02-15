54096781ca7fdfe99f92015a73751e839e3412a4
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2025-02-15 23:35:59 +0530

    added WTForms

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my super secret key'
+
+# Create a form class
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 # Define a route for the home page
 @app.route('/')
@@ -25,3 +34,14 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+# Create name page
+@app.route('/name', methods=['GET', 'POST'])
+
+def name():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html', name=name, form=form)
